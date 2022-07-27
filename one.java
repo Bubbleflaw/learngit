4dc2ed2d250d366d24c4241934b88e5de67ea29e
Bubbleflaw <1942044459@qq.com>
2022-07-27 15:35:28 +0800

    java.class

---
+++ one.java
@@ -0,0 +1,10 @@
+/**
+ * @Author:黄文倩
+ * @CreatTime:2022/7/27
+ * @Description:
+ */
+public class one {
+    public static void main(String[] args) {
+        System.out.println("i want to say ");
+    }
+}
